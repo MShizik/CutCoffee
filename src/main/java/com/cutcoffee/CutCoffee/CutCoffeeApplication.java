acf2712c4039726a0a7d352ab71b9c39428f2488
MShizikU <125292382+MShizikU@users.noreply.github.com>
2023-05-22 15:24:39 +0300

    Initial commit

---
+++ src/main/java/com/cutcoffee/CutCoffee/CutCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.cutcoffee.CutCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CutCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CutCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cutcoffee/CutCoffee/CutCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cutcoffee.CutCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
